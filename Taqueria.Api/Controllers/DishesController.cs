97cdbcee007274c9e7379fe3fcfe3cce2395c9c3
rodrigoBojorquez <rbojorquez1620@gmail.com>
2025-06-20 14:09:14 -0500

    feat(dish): implementacion de servicio y controlador

--- Taqueria.Api/Controllers/DishesController.cs
+++ Taqueria.Api/Controllers/DishesController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Taqueria.Api.Services.Dish;
 
@@ -13,6 +14,12 @@ public class DishesController(DishService dishService) : ControllerBase
         decimal Price
     );
     
+    public record UpdateDishRequest(
+        string Name,
+        string? Description,
+        decimal Price
+    );
+    
     
     [HttpPost]
     public async Task<IActionResult> CreateDish([FromBody] CreateDishRequest request)
@@ -23,5 +30,27 @@ public class DishesController(DishService dishService) : ControllerBase
             request.Price
         );
         return Ok("Platillo creado exitosamente");
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetAllDishes()
+    {
+        var dishes = await dishService.GetAllAsync();
+        return Ok(dishes);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update([FromBody] UpdateDishRequest request, Guid id)
+    {
+        // Implement update logic here
+        await dishService.UpdateAsync(id, request.Name, request.Description, request.Price);
+        return Ok();
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        await dishService.DeleteAsync(id);
+        return Ok("Platillo eliminado exitosamente");
     }
 }
--- Taqueria.Api/Services/Dish/DishService.cs
+++ Taqueria.Api/Services/Dish/DishService.cs
@@ -2,7 +2,7 @@ namespace Taqueria.Api.Services.Dish;
 
 public class DishService(IDishRepository dishRepository)
 {
-    public async Task CreateAsync(string name, string? description, decimal price)
+        public async Task CreateAsync(string name, string? description, decimal price)
     {
         var dish = new Data.Entities.Dish
         {
@@ -12,5 +12,30 @@ public class DishService(IDishRepository dishRepository)
         };
 
         await dishRepository.AddAsync(dish);
+    }
+    
+    public async Task<List<Data.Entities.Dish>> GetAllAsync()
+    {
+        return await dishRepository.GetAllAsync();
+    }
+    
+    public async Task UpdateAsync(Guid id, string name, string? description, decimal price)
+    {
+        var dish = await dishRepository.GetByIdAsync(id);
+        if (dish == null)
+        {
+            throw new KeyNotFoundException("Dish not found");
+        }
+
+        dish.Name = name;
+        dish.Description = description;
+        dish.Price = price;
+
+        await dishRepository.UpdateAsync(dish);
+    }
+    
+    public async Task DeleteAsync(Guid id)
+    {
+        await dishRepository.HardDeleteAsync(id);
     }
 }
